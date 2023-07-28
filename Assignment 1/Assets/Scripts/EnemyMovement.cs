fda22d9feafcade423ec237130888e82346389c9
Amro <2592997@students.wits.ac.za>
2023-07-28 20:37:35 +0200

    Enemy Movement Fixed

--- Assignment 1/Assets/Scripts/EnemyMovement.cs
+++ Assignment 1/Assets/Scripts/EnemyMovement.cs
@@ -14,7 +14,7 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPos = new Vector2(target.transform.position.x, transform.position.y);
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, 0f);
+        Vector2 targetPos = new Vector2(target.transform.position.x, target.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, 1.4f * Time.deltaTime);
     }
 }
